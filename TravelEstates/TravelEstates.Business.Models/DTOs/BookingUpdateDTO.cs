4022cbeda136f976cf4a2d6160bbba683ff3fdd4
nedkom21 <nedkom21@gmail.com>
2023-09-30 21:41:45 +0300

    Booking DTOs and IMapper profile added added

---
+++ TravelEstates/TravelEstates.Business.Models/DTOs/BookingUpdateDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace TravelEstates.Business.Models.DTOs
+{
+    public class BookingUpdateDTO
+    {
+        public string Id { get; set; }
+
+        public string RentPropertyId { get; set; }
+
+        public string UserId { get; set; }
+
+        public DateTime CheckInDate { get; set; }
+
+        public DateTime CheckOutDate { get; set; }
+    }
+}
