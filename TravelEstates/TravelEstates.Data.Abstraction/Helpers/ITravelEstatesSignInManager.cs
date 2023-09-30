76a2a2b974baea879f8193ccf62bca6e7c54c64f
nedkom21 <nedkom21@gmail.com>
2023-09-30 21:50:00 +0300

    Added SignIn and UserManager

---
+++ TravelEstates/TravelEstates.Data.Abstraction/Helpers/ITravelEstatesSignInManager.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TravelEstates.Data.Models.Entities.Base;
+
+namespace TravelEstates.Data.Abstraction.Helpers
+{
+    public interface ITravelEstatesSignInManager
+    {
+        Task<SignInResult> PasswordSignInAsync(User user, string password, bool isPersistent, bool lockoutOnFailure);
+
+        Task SignOutAsync();
+    }
+}
---
+++ TravelEstates/TravelEstates.Data.Abstraction/Helpers/ITravelEstatesUserManager.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TravelEstates.Data.Models.Entities.Base;
+
+namespace TravelEstates.Data.Abstraction.Helpers
+{
+    public interface ITravelEstatesUserManager
+    {
+        Task CreateAdminAsync(User user, string password);
+
+        Task CreateUserAsync(User user, string password);
+
+        Task<User> FindByEmailAsync(string email);
+
+        Task<User> FindUserByIdAsync(string userId);
+
+        Task<IEnumerable<string>> GetRolesAsync(User user);
+
+        Task<string> GenerateConfirmEmailTokenAsync(User user);
+
+        Task<IdentityResult> ConfirmEmailAsync(User user, string token);
+
+        Task<string> GenerateResetPasswordTokenAsync(User user);
+
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string newPassword, string confirmPassword);
+
+        Task<bool> IsValidTokenAsync(User user, string tokenProvider, string purpose, string token);
+    }
+}
