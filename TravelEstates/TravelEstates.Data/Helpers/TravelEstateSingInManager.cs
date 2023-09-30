f932a3c6b427ab260c8849e15868b7fedfa74474
nedkom21 <nedkom21@gmail.com>
2023-09-30 21:42:14 +0300

    SignInManager and UserManager modified

--- TravelEstates/TravelEstates.Data/Helpers/TravelEstateSingInManager.cs
+++ TravelEstates/TravelEstates.Data/Helpers/TravelEstateSingInManager.cs
@@ -1,8 +1,28 @@
-﻿using TravelEstates.Data.Abstraction.Repositories.Helpers;
+﻿using Microsoft.AspNetCore.Identity;
+using TravelEstates.Data.Abstraction.Helpers;
+using TravelEstates.Data.Models.Entities.Base;
 
 namespace TravelEstates.Data.Helpers
 {
-    public class TravelEstateSingInManager : ITravelEstateUserManager
+    public class TravelEstateSingInManager : ITravelEstatesSignInManager
     {
+        private readonly SignInManager<User> _signInManager;
+
+        public TravelEstateSingInManager(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<SignInResult> PasswordSignInAsync(User user, string password, bool isPersistant, bool lockoutOnFailure)
+        {
+            var result = await _signInManager.PasswordSignInAsync(user, password, isPersistant, lockoutOnFailure);
+
+            return result;
+        }
+
+        public async Task SignOutAsync()
+        {
+            await _signInManager.SignOutAsync();
+        }
     }
 }
