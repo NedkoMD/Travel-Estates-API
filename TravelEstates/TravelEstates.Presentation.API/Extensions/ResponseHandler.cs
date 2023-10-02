6c24cda876686e25ae8cda4dd24b9ee35c159a97
nedkom21 <nedkom21@gmail.com>
2023-10-02 19:14:12 +0300

    Added Controller and Service for Booking

---
+++ TravelEstates/TravelEstates.Presentation.API/Extensions/ResponseHandler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelEstates.Business.Models.Enums;
+using TravelEstates.Business.Models.Results.Base;
+
+namespace TravelEstates.Presentation.API.Extensions
+{
+    public static class ResponseHandler
+    {
+        public static IActionResult HandleResponse<T>(this ControllerBase controller, IResult<T> response)
+        {
+            if (response.StatusCode == TravelEstatesStatusCode.NotFound)
+            {
+                return controller.NotFound(response.ErrorMessages);
+            }
+            else if (response.StatusCode == TravelEstatesStatusCode.BadRequest)
+            {
+                return controller.BadRequest(response.ErrorMessages);
+            }
+            else if (response.StatusCode == TravelEstatesStatusCode.Unauthorized)
+            {
+                return controller.Unauthorized(response.ErrorMessages);
+            }
+            else if (response.StatusCode == TravelEstatesStatusCode.Forbidden)
+            {
+                return controller.Forbid();
+            }
+            else if (response.StatusCode == TravelEstatesStatusCode.OK)
+            {
+                return controller.Ok(response.Data);
+            }
+            else if (response.StatusCode == TravelEstatesStatusCode.NoContent)
+            {
+                return controller.NoContent();
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
