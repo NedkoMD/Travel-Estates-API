510cc79fefe943b16ca47d17eba014a31a5c4d4b
nedkom21 <nedkom21@gmail.com>
2023-10-05 18:03:33 +0300

    PropertyTypeRepository added

---
+++ TravelEstates/TravelEstates.Data.Abstraction/Repositories/IPropertyTypeRepository.cs
@@ -0,0 +1,9 @@
+﻿using TravelEstates.Data.Abstraction.Repositories.Base;
+using TravelEstates.Data.Models.Entities.Base;
+
+namespace TravelEstates.Data.Abstraction.Repositories
+{
+    public interface IPropertyTypeRepository : IRepository<PropertyType>
+    {
+    }
+}
